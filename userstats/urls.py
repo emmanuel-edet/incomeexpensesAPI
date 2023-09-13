29211deffa15e660fbdd9977cdedecd5388510e7
Emmanuel O. Edet <eoedet@gzican.com>
2023-09-13 17:53:38 +0100

    ch(app): add all code

---
+++ userstats/urls.py
@@ -0,0 +1,10 @@
+from .views import ExpenseSummaryStats, IncomeSummaryStats
+from django.urls import path
+
+
+urlpatterns = [
+    path('expense_category_data', ExpenseSummaryStats.as_view(),
+         name='expense-category-summary'),
+    path('income_source_data', IncomeSummaryStats.as_view(),
+         name='income-sources-summary'),
+]
